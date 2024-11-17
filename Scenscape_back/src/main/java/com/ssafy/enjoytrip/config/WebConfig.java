b5ae6cb7e2bf5513725c73b11d3603c9a1bdf000
이주현 <juhyun@ijuhyeon-ui-MacBookAir.local>
2024-11-17 15:56:45 +0900

    fix: localhost:5173 CORS 추가 설정

--- Scenscape_back/src/main/java/com/ssafy/enjoytrip/config/WebConfig.java
+++ Scenscape_back/src/main/java/com/ssafy/enjoytrip/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500","http://localhost:5173", "http://localhost:5174")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
